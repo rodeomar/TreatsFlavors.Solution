520ab304df1262ecbe8d43b9be23f08d66beef41
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-13 17:13:56 +0530

    Added TreatFlavors class

--- TreatsFlavors/Models/Flavor.cs
+++ TreatsFlavors/Models/Flavor.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<TreatFlavor> TreatFlavors { get; set; }
+
+
     }
 }
---
+++ TreatsFlavors/Models/TreatFlavor.cs
@@ -0,0 +1,11 @@
+﻿namespace TreatsFlavors.Models
+{
+    public class TreatFlavor
+    {
+        public int TreatId { get; set; }
+        public Treat Treat { get; set; }
+
+        public int FlavorId { get; set; }
+        public Flavor Flavor { get; set; }
+    }
+}
