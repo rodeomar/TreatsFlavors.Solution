59e8e7b3fe0b36ec74eab3be9478173ddb90ee33
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-13 19:57:53 +0530

    Added TreatsController

---
+++ TreatsFlavors/Controllers/TreatsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TreatsFlavors.Models;
+
+namespace TreatsFlavors.Controllers
+{
+    public class TreatsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TreatsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var treats = _context.Treats.ToList();
+            return View(treats);
+        }
+
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Name")] Treat treat)
+        {
+            
+            _context.Add(treat);
+            _context.SaveChanges();
+            
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
