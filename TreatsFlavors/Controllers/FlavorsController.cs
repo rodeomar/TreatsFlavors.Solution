756f78625bb3d9decbd9a149ed2f72e20319c7b1
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-14 08:59:42 +0530

    Added FlavorsController and Index View

---
+++ TreatsFlavors/Controllers/FlavorsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using TreatsFlavors.Models;
+
+namespace TreatsFlavors.Controllers
+{
+    public class FlavorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FlavorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var flavors = _context.Flavors.ToList();
+            return View(flavors);
+        }
+    }
+}
