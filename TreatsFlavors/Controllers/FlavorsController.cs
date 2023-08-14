dd5e1d23281c6584d4a9210d80af98099bb63270
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-14 11:24:45 +0530

    Added Add Treat Functionality for Flavors

--- TreatsFlavors/Controllers/FlavorsController.cs
+++ TreatsFlavors/Controllers/FlavorsController.cs
@@ -46,6 +46,34 @@ namespace TreatsFlavors.Controllers
 
             return View(new { Flavor = flavor, Context = _context });
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddTreat(int flavorId, int treatId)
+        {
+            var flavor = await _context.Flavors.Include(f => f.TreatFlavors).FirstOrDefaultAsync(f => f.Id == flavorId);
+            var treat = await _context.Treats.FirstOrDefaultAsync(t => t.Id == treatId);
+
+            if (flavor != null || treat != null)
+            {
+                var treatFlavor = new TreatFlavor { FlavorId = flavorId, TreatId = treatId };
+
+
+                try
+                {
+                    _context.TreatFlavors.Add(treatFlavor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (InvalidOperationException)
+                {
+                    return RedirectToAction("Details", new { id = flavorId });
+                }
+
+            }
+
+            return RedirectToAction("Details", new { id = flavorId });
+        }
+
+
 
 
         public async Task<IActionResult> Delete(int id)
